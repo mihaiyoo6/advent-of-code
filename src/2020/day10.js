6e73964e935919146da06d825d6432736a4a47e4
Mihai Mijea <mijeamihai@yahoo.com>
2021-02-19 12:00:27 +0100

    Day 11 a

--- src/2020/day10.js
+++ src/2020/day10.js
@@ -2,18 +2,38 @@ const { fetchData } = require("../utils");
 const main = async () => {
   const data = await fetchData("10");
   const mockData = `
-16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4`;
-  const cleanData = mockData
+  28
+  33
+  18
+  42
+  31
+  14
+  46
+  20
+  48
+  47
+  24
+  23
+  49
+  45
+  19
+  38
+  39
+  11
+  1
+  32
+  25
+  35
+  8
+  17
+  7
+  9
+  4
+  2
+  34
+  10
+  3`;
+  const cleanData = data
     .trim()
     .split(`\n`)
     .filter((i) => i)
@@ -46,6 +66,29 @@ const runA = (data) => {
   console.log({ ones, trees });
   return ones * trees;
 };
-const runB = (data) => {};
-main().then((r) => console.log(JSON.stringify(r, null, 2)));
+
+const canBeSkipped = (arr, i) => arr[i + 1] - arr[i - 1] <= 3;
+const findPosibilities = (arr, memo = {}) => {
+  const key = arr.join`,`;
+  if (key in memo) {
+    return memo[key];
+  }
+  let result = 1;
+  for (let i = 1; i < arr.length - 1; i++) {
+    if (canBeSkipped(arr, i)) {
+      result += findPosibilities([arr[i - 1]].concat(arr.slice(i + 1)), memo);
+    }
+  }
+  memo[key] = result;
+  return result;
+};
+
+const runB = (data) => {
+  const adapters = data.sort((a, b) => a - b);
+  const deciceAdapter = Math.max(...adapters) + 3;
+  const allAdapters = [0, ...adapters, deciceAdapter];
+
+  return findPosibilities(allAdapters);
+};
+// main().then((r) => console.log(JSON.stringify(r, null, 2)));
 module.exports = main;
--- src/2020/day11.js
+++ src/2020/day11.js
@@ -1,13 +1,77 @@
 const { fetchData } = require("../utils");
 const main = async () => {
   const data = await fetchData("11");
+  const mockData = `
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`;
+  const cleanData = data.split("\n").filter((item) => item);
 
   return {
-    day11a: runA(data.split("\n").filter((item) => item)),
-    day11b: runB(data.split("\n").filter((item) => item))
+    day11a: runA(cleanData),
+    day11b: runB(cleanData)
   };
 };
-const runA = (data) => {};
+
+const neighbors = (arr, m, n) => {
+  // define what a neighbor is
+  const v = [
+    [0, 1], // right
+    [1, 0], // down
+    [0, -1], // left
+    [-1, 0], // up
+    [1, 1], // down-right
+    [-1, -1], // up-left
+    [-1, 1], // up-right
+    [1, -1] // down-left
+  ];
+  // filter edges & map
+  return v
+    .filter(
+      ([h, j]) =>
+        h + m >= 0 && h + m < arr.length && j + n >= 0 && j + n < arr[0].length
+    )
+    .map(([h, j]) => arr[h + m][j + n]);
+};
+
+// If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+// If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
+// Otherwise, the seat's state does not change.
+const applyRules = (arr) => {
+  const newState = arr.map((line, y) => {
+    return [...line]
+      .map((seat, x) => {
+        const ocupiedAdiacentSeats = neighbors(arr, y, x).filter(
+          (i) => i === "#"
+        ).length;
+        if (seat === "#" && ocupiedAdiacentSeats >= 4) {
+          //seat empties
+          return "L";
+        }
+        if (seat === "L" && ocupiedAdiacentSeats === 0) {
+          //seat occupies
+          return "#";
+        }
+        return seat;
+      })
+      .reduce((a, b) => a + b, "");
+  });
+  if (arr.join() !== newState.join()) {
+    return applyRules(newState);
+  }
+  return newState;
+};
+const runA = (data) => {
+  const finalState = applyRules(data);
+  return finalState.reduce((a, b) => a + (b.match(/#/g) || []).length, 0);
+};
 const runB = (data) => {};
-// main().then((r) => console.log(JSON.stringify(r, null, 2)));
+main().then((r) => console.log(JSON.stringify(r, null, 2)));
 module.exports = main;
